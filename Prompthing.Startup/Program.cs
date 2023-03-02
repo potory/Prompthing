2db03754ef46126543903ec1d01861a904d0e322
Alexey Potory <>
2023-03-02 21:04:28 +0500

    Добавил примеры

--- Prompthing.Startup/Program.cs
+++ Prompthing.Startup/Program.cs
@@ -1,8 +1,16 @@
-﻿using Prompthing.Core;
+﻿using System.Text;
 using Prompthing.Core.Templates;
 
 string content = File.ReadAllText("Example.json");
 
+var stringBuilder = new StringBuilder();
 var templates = new DatasetCompiler().Compile(content);
+
+for (int i = 0; i < 10; i++)
+{
+    templates[0].Node.Evaluate(stringBuilder);
+    Console.WriteLine(stringBuilder.ToString());
+    stringBuilder.Clear();
+}
 
 Console.ReadKey();
