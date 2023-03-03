2c94d355c267b2dd3cec1c100a94b3653ea5bd43
Alexey Potory <>
2023-03-03 17:24:37 +0500

    Добавил заготовки шаблонов

---
+++ Prompthing.Core/PredefinedTemplates.cs
@@ -0,0 +1,44 @@
+﻿using Newtonsoft.Json.Linq;
+
+namespace Prompthing.Core;
+
+public static class PredefinedTemplates
+{
+    public static string JsonDataset()
+    {
+        JObject jsonObject = new JObject(
+            new JProperty("templates",
+                new JArray(
+                    new JObject(
+                        new JProperty("template", "a {{gender}} holding {{item}}")
+                    )
+                )
+            ),
+            new JProperty("categories",
+                new JArray(
+                    new JObject(
+                        new JProperty("name", "gender"),
+                        new JProperty("values",
+                            new JArray(
+                                "men",
+                                "woman"
+                            )
+                        )
+                    ),
+                    new JObject(
+                        new JProperty("name", "item"),
+                        new JProperty("values",
+                            new JArray(
+                                "book",
+                                "glass",
+                                "flag"
+                            )
+                        )
+                    )
+                )
+            )
+        );
+
+        return jsonObject.ToString();
+    }
+}
