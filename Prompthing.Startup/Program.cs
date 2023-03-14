3da7cfbc2ec812871f036c811d4b2821f3f99c99
Alexey Potory <>
2023-03-14 13:23:28 +0500

    Фикс стартап проекта

--- Prompthing.Startup/Program.cs
+++ Prompthing.Startup/Program.cs
@@ -8,7 +8,7 @@ var templates = new DatasetCompiler().Compile(content);
 
 for (int i = 0; i < 10; i++)
 {
-    templates[0].Node.Evaluate(stringBuilder);
+    templates.Templates[0].Node.Evaluate(stringBuilder);
     Console.WriteLine(stringBuilder.ToString());
     stringBuilder.Clear();
 }
